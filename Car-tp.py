501cf310cf9b3160e23491ca5ab83507b01b7da0
Parasdave420 <43946278+Parasdave420@users.noreply.github.com>
2021-01-17 20:07:21 +0530

    Create Car-tp.py

---
+++ Car-tp.py
@@ -0,0 +1,36 @@
+#This is normal while loop and other stuffs implementation code.
+#simple command line game.
+
+command = ""
+started = False
+
+while True:
+    command = input(">").lower()
+    
+    if command == "start":
+        if started:
+            print("Car is already started.")
+        else:
+            started = True
+            print("Car started...")
+    
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car stopped.")
+    
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit = to quit
+        """)
+    
+    elif command == "quit":
+        break
+    
+    else:
+        print("Soory! I didn't get you...")
+        
