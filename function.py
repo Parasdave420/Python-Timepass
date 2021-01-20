55664807dbf23bde9cc97c01c8dbde5e0d58a46e
Parasdave420 <43946278+Parasdave420@users.noreply.github.com>
2021-01-20 23:42:32 +0530

    Update function.py

--- function.py
+++ function.py
@@ -1,10 +1,10 @@
 # Simple implementation of function in python
 
-def greet_user():
-    print('Hi there')
+def greet_user(name):
+    print(f'Hi {name}!')
     print('Welcome here!!!')
     
 
 print("Start the function call")
-greet_user()
+greet_user(input("Enter your name: "))
 print("Function called successfully")
