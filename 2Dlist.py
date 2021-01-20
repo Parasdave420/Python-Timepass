8d673e5e87f690c9fbb99a3ad1939c87a644b00f
Parasdave420 <43946278+Parasdave420@users.noreply.github.com>
2021-01-20 19:34:27 +0530

    Update 2Dlist.py

--- 2Dlist.py
+++ 2Dlist.py
@@ -6,5 +6,12 @@ matrix = [
     [1, 2, 4],
     [9, 1, 0]
     ]
-    
+
+#print specific item
 print(matrix[0][2])
+
+#iterate whole matrix
+for row in matrix:
+    for item in row:
+        print(item)
+    
