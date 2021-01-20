0ea9c9cb3eb24fa17477d7f7ed623b23e5576b0e
Parasdave420 <43946278+Parasdave420@users.noreply.github.com>
2021-01-20 23:31:11 +0530

    Create function.py
    Implementation of function in python

---
+++ function.py
@@ -0,0 +1,10 @@
+# Simple implementation of function in python
+
+def greet_user():
+    print('Hi there')
+    print('Welcome here!!!')
+    
+
+print("Start the function call")
+greet_user()
+print("Function called successfully")
